f31450fcceb3b695380df42717899b3a591c7154
Enlite Tech <36366929+enlite-tech@users.noreply.github.com>
2018-03-24 09:18:19 +0530

    interview.js

---
+++ interview.js
@@ -0,0 +1,14 @@
+Stack holder (means who give you requirement)
+why we use mongodb, compiment of mongo
+what is rest artitecture.
+my sql interview question.
+how to implement worker in javascript.
+what is closures in javascript.
+what is event loop.
+different between es5 and es6.
+life cycle of angular2.
+life cycle of react native.
+mongo compound index.
+firstElement
+
+All type of mysql query
